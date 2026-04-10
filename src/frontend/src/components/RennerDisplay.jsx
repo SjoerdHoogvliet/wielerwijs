3c1022127fbe59b689a03299864f5781e57605b4
Sjoerd <sjoerd.hoogvliet@student.hu.nl>
2026-04-10 19:41:19 +0200

    Make rennerDisplay horizontal instead of square

--- src/frontend/src/components/RennerDisplay.jsx
+++ src/frontend/src/components/RennerDisplay.jsx
@@ -1,9 +1,11 @@
 export default function RennerDisplay(props) {
   return (
-    <div 
-        onClick={props.function} 
-        className="hover:cursor-pointer aspect-square w-42 h-42 rounded-md bg-gray-100 flex justify-center items-center">
-      <h2>{props.renner.naam}</h2>
+    <div className="w-lg p-6 rounded-md bg-gray-100 flex">
+      <p>{props.renner.naam}</p>
+      <p
+        onClick={props.function}  
+        className=" ml-auto text-2xl hover:cursor-pointer hover:scale-150 duration-150 align-middle"
+      > {props.removal ? "-" : "+"} </p>
     </div>
   )
 }

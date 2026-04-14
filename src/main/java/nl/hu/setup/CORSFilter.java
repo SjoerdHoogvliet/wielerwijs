2b53b514a13ca61eb3a720cf24de5486d014fb5a
Sjoerd <sjoerd.hoogvliet@student.hu.nl>
2026-04-14 16:11:06 +0200

    Change Add into putSingle to make sure duplication does not occur

--- src/main/java/nl/hu/setup/CORSFilter.java
+++ src/main/java/nl/hu/setup/CORSFilter.java
@@ -8,24 +8,26 @@ package nl.hu.setup;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 
 @Provider
+@PreMatching
 public class CORSFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().add(
+        responseContext.getHeaders().putSingle(
                 "Access-Control-Allow-Origin", "*"
         );
-        responseContext.getHeaders().add(
-                "Access-Control-Allow-Headers", "origin, content-type, accept, authorization"
+        responseContext.getHeaders().putSingle(
+                "Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization"
         );
-        responseContext.getHeaders().add(
+        responseContext.getHeaders().putSingle(
                 "Access-Control-Allow-Credentials", "true"
         );
-        responseContext.getHeaders().add(
+        responseContext.getHeaders().putSingle(
                 "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD"
         );
     }

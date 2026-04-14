3f1159b456982cdeb4d40500bd3691ec0feb1f31
Sjoerd <sjoerd.hoogvliet@student.hu.nl>
2026-04-14 16:15:11 +0200

    Improve rennerDisplay to link to rider page

--- src/frontend/src/components/RennerDisplay.jsx
+++ src/frontend/src/components/RennerDisplay.jsx
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router";
+
 export default function RennerDisplay(props) {
+  let navigate = useNavigate();
+
   return (
-    <div className="w-lg p-6 rounded-md bg-gray-100 flex">
+    <div className="w-lg p-6 rounded-md bg-gray-100 flex hover:cursor-pointer" onClick={() => navigate(`/renner/${props.renner.id}`)}>
       <p>{props.renner.naam}</p>
       <p
         onClick={props.function}  

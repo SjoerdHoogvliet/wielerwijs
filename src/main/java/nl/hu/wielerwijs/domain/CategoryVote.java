255845f653bfaac3d9125095e54835d766500323
Sjoerd <sjoerd.hoogvliet@student.hu.nl>
2026-04-14 17:22:27 +0200

    Return to class for additional flexibility

--- src/main/java/nl/hu/wielerwijs/domain/CategoryVote.java
+++ src/main/java/nl/hu/wielerwijs/domain/CategoryVote.java
@@ -2,4 +2,26 @@ package nl.hu.wielerwijs.domain;
 
 import nl.hu.security.domain.User;
 
-public record CategoryVote(String category, User user, Renner renner) {}
+public class CategoryVote {
+    private String category;
+    private User user;
+    private Renner renner;
+
+    public CategoryVote(String category, User user, Renner renner) {
+        this.category = category;
+        this.user = user;
+        this.renner = renner;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Renner getRenner() {
+        return renner;
+    }
+}

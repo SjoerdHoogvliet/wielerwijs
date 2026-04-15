e219d7d508575caada1bb1ec9de8af781ff833c8
Sjoerd <sjoerd.hoogvliet@student.hu.nl>
2026-04-15 20:06:27 +0200

    Add the option to display in card and not

--- src/frontend/src/components/RennerDisplay.jsx
+++ src/frontend/src/components/RennerDisplay.jsx
@@ -3,13 +3,27 @@ import { useNavigate } from "react-router";
 export default function RennerDisplay(props) {
   let navigate = useNavigate();
 
-  return (
-    <div className="w-lg p-6 rounded-md bg-gray-100 flex hover:cursor-pointer" onClick={() => navigate(`/renner/${props.renner.id}`)}>
-      <p>{props.renner.naam}</p>
-      <p
-        onClick={props.function}  
-        className=" ml-auto text-2xl hover:cursor-pointer hover:scale-150 duration-150 align-middle"
-      > {props.removal ? "-" : "+"} </p>
-    </div>
-  )
+  if (props.card) {
+    return (
+      <div 
+        className="w-lg p-6 rounded-md bg-gray-100 flex hover:cursor-pointer" 
+        onClick={() => navigate(`/renner/${props.renner.id}`)}
+      >
+        <p>{props.renner.naam}</p>
+        <p
+          onClick={props.function}  
+          className=" ml-auto text-2xl hover:cursor-pointer hover:scale-150 duration-150 align-middle"
+        > {props.removal ? "-" : "+"} </p>
+      </div>
+    )
+  } else {
+    return (
+      <span
+        className="text-secondary hover:font-bold duration-150 hover:cursor-pointer"
+        onClick={() => navigate(`/renner/${props.renner.id}`)}
+      >
+        {props.renner.naam}
+      </span>
+    )
+  }
 }

70b0bd6dc1e447aa96b5cd8b3147b14f22e3d056
Sjoerd <sjoerd.hoogvliet@student.hu.nl>
2026-04-15 20:07:31 +0200

    add square / allowRemoval + move logic

--- src/frontend/src/pages/DisplayTeams.jsx
+++ src/frontend/src/pages/DisplayTeams.jsx
@@ -23,51 +23,13 @@ export default function DisplayTeams() {
         loadData()
     }, [])
 
-    function likeTeam(id) {
-        fetch('http://localhost:8080/api/team/' + id, {
-            method: 'PUT',
-            headers: {
-                'Authorization': `Bearer ${sessionStorage.getItem("jwtToken")}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ like: true })
-        })
-
-        loadData()
-    }
-
-    function dislikeTeam(id) {
-        fetch('http://localhost:8080/api/team/' + id, {
-            method: 'PUT',
-            headers: {
-                'Authorization': `Bearer ${sessionStorage.getItem("jwtToken")}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ like: false })
-        })
-
-        loadData()
-    }
-
-    function removeTeam(id) {
-        fetch('http://localhost:8080/api/team/' + id, {
-            method: 'DELETE',
-            headers: {
-                'Authorization': `Bearer ${sessionStorage.getItem("jwtToken")}`,
-                'Content-Type': 'application/json'
-            }
-        })
-
-        loadData()
-    }
-
     return (
         <div className="mx-32">
             <div className="flex w-full">
                 <div>
                     <h2 className="text-4xl p-4">Teams</h2>
                     <div className="grid-cols-4 grid space-y-8 space-x-8 px-4">
-                        {teams.map(team => <TeamDisplay team={team} likeTeam={() => likeTeam(team.id)} dislikeTeam={() => dislikeTeam(team.id)} removeTeam={() => removeTeam(team.id)} />)}
+                        {teams.map(team => <TeamDisplay team={team} square allowRemoval />)}
                     </div>
                 </div>
             </div>

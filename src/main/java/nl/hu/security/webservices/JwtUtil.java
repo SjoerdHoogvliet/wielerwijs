28f8bab55ea8190d78732fb386042e0828887ca6
Sjoerd <sjoerd.hoogvliet@student.hu.nl>
2026-04-19 01:04:32 +0200

    Add token functions

--- src/main/java/nl/hu/security/webservices/JwtUtil.java
+++ src/main/java/nl/hu/security/webservices/JwtUtil.java
@@ -7,10 +7,10 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
 
 public class JwtUtil {
-    public static String generateToken(String username, String role) {
+    public static String generateToken(String username, UserRole role) {
         return Jwts.builder()
                 .setSubject(username)
-                .claim("role", role)
+                .claim("role", role.name())
                 .setIssuedAt(new Date())
                 .signWith(SignatureAlgorithm.HS512, "this-is-a-512-bit-key-used-for-signing-jwt-tokens-that-should-be-changed-production".getBytes())
                 .compact();
@@ -21,5 +21,15 @@ public class JwtUtil {
                 .setSigningKey("this-is-a-512-bit-key-used-for-signing-jwt-tokens-that-should-be-changed-production".getBytes())
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    public static String getUsername(String token) {
+        Claims claims = validateToken(token);
+        return claims.getSubject();
+    }
+
+    public static UserRole getRole(String token) {
+        Claims claims = validateToken(token);
+        return UserRole.valueOf(claims.get("role", String.class));
     }
 }
